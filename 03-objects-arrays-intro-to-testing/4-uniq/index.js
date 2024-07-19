737fbc6f0e75619b4f5279848c0596342b92386f
SergeyR013 <56975358+SergeyR013@users.noreply.github.com>
2024-07-19 10:13:12 +0300

    Решение задачи uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -4,5 +4,8 @@
  * @returns {*[]} - the new array with uniq values
  */
 export function uniq(arr) {
+  if (arr === undefined || arr.length == 0)
+    return [];
 
+  return Array.from(new Set(arr));
 }
